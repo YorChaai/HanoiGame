d6bab70ee3019c3661b84a3b3d1d8185413b3f33
YorChaai <diofavianrch@gmail.com>
2025-10-02 22:19:29 +0700

    gamehanoi2

--- hanoiGame/app/routes.py
+++ hanoiGame/app/routes.py
@@ -1,14 +1,14 @@
 # app/routes.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def home():
-    # Kita kembalikan ke template, karena tes sudah berhasil
+    # INI ADALAH BARIS YANG PALING KRUSIAL
+    # Harus mengembalikan render_template('index.html')
     return render_template('index.html')
 
-# PASTIKAN FUNGSI INI ADA DAN BENAR
 @main_bp.route('/about')
 def about():
-    return "<h1>Ini Halaman About</h1>"
+    return '<h1>Ini Halaman About</h1><br><a href="' + url_for('main.home') + '">Kembali ke Beranda</a>'
